eff500084626b1a7f9894f0d4ce7cafa4758a556
NicknameNeo <abdulloevaslan96@gmail.com>
2024-11-24 13:33:37 +0300

    My project

---
+++ dars case/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace dars_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = Convert.ToInt32(Console.ReadLine());
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Not a day");
+                    break;
+            }
+        }
+    }
+}
